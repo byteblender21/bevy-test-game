17420828da3078f43e924c37311ba1259b779341
ByteBlender <byteblender21@gmail.com>
2023-06-14 18:33:15 +0200

    Add simple 3d hex grid

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 use bevy::prelude::*;
+use bevy::render::mesh::Indices;
+use bevy::render::render_resource::PrimitiveTopology;
+use bevy::time::common_conditions::on_timer;
+use hexx::shapes;
+use hexx::*;
+use std::collections::HashMap;
+use std::time::Duration;
 use leafwing_input_manager::prelude::*;
+
+/// World size of the hexagons (outer radius)
+const HEX_SIZE: Vec2 = Vec2::splat(1.0);
+/// World space height of hex columns
+const COLUMN_HEIGHT: f32 = 1.0;
+/// Map radius
+const MAP_RADIUS: u32 = 20;
+/// Animation time step
+const TIME_STEP: Duration = Duration::from_millis(100);
 
 // This is the list of "things in the game I want to be able to do based on input"
 #[derive(Actionlike, PartialEq, Eq, Clone, Copy, Hash, Debug)]
@@ -23,7 +39,73 @@ fn main() {
         .add_system(jump)
         // setup env
         .add_startup_system(setup)
+        .add_startup_system(setup_grid)
         .run();
+}
+
+fn hexagonal_column(hex_layout: &HexLayout) -> Mesh {
+    let mesh_info = ColumnMeshBuilder::new(hex_layout, COLUMN_HEIGHT)
+        .without_bottom_face()
+        .build();
+    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+    mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, mesh_info.vertices);
+    mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, mesh_info.normals);
+    mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, mesh_info.uvs);
+    mesh.set_indices(Some(Indices::U16(mesh_info.indices)));
+    mesh
+}
+
+#[derive(Debug, Resource)]
+struct Map {
+    entities: HashMap<Hex, Entity>,
+    highlighted_material: Handle<StandardMaterial>,
+    default_material: Handle<StandardMaterial>,
+}
+
+#[derive(Debug, Default, Resource)]
+struct HighlightedHexes {
+    ring: u32,
+    hexes: Vec<Hex>,
+}
+
+/// Hex grid setup
+fn setup_grid(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let layout = HexLayout {
+        hex_size: Vec2::new(0.1, 0.1),
+        orientation: HexOrientation::flat(),
+        ..default()
+    };
+    // materials
+    let default_material = materials.add(Color::WHITE.into());
+    let highlighted_material = materials.add(Color::YELLOW.into());
+    // mesh
+    let mesh = hexagonal_column(&layout);
+    let mesh_handle = meshes.add(mesh);
+
+    let entities = shapes::hexagon(Hex::ZERO, 20)
+        .map(|hex| {
+            let pos = layout.hex_to_world_pos(hex);
+            let id = commands
+                .spawn(PbrBundle {
+                    transform: Transform::from_xyz(pos.x, -0.2, pos.y)
+                        .with_scale(Vec3::new(1.0, 0.1, 1.0)),
+                    mesh: mesh_handle.clone(),
+                    material: default_material.clone(),
+                    ..default()
+                })
+                .id();
+            (hex, id)
+        })
+        .collect();
+    commands.insert_resource(Map {
+        entities,
+        highlighted_material,
+        default_material,
+    });
 }
 
 /// set up a simple 3D scene
@@ -34,11 +116,12 @@ fn setup(
     asset_server: Res<AssetServer>
 ) {
     // plane
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(shape::Plane::from_size(5.0).into()),
-        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-        ..default()
-    });
+    // commands.spawn(PbrBundle {
+    //     mesh: meshes.add(shape::Plane::from_size(5.0).into()),
+    //     material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+    //     ..default()
+    // });
+    // plane
 
     commands.spawn(SceneBundle {
         scene: asset_server.load("models/house.gltf#Scene0"),
