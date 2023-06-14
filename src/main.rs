822dc9a9cdb64a358a9037f67e4505c7a53e4e80
ByteBlender <byteblender21@gmail.com>
2023-06-14 18:14:50 +0200

    Add way to handle input with component

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
 use bevy::prelude::*;
+use leafwing_input_manager::prelude::*;
+
+// This is the list of "things in the game I want to be able to do based on input"
+#[derive(Actionlike, PartialEq, Eq, Clone, Copy, Hash, Debug)]
+enum Action {
+    Run,
+    Jump,
+}
+
+#[derive(Component)]
+struct Player;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        // This plugin maps inputs to an input-type agnostic action-state
+        // We need to provide it with an enum which stores the possible actions a player could take
+        .add_plugin(InputManagerPlugin::<Action>::default())
+        // The InputMap and ActionState components will be added to any entity with the Player component
+        .add_startup_system(spawn_player)
+        // Read the ActionState in your systems using queries!
+        .add_system(jump)
+        // setup env
         .add_startup_system(setup)
         .run();
 }
@@ -28,18 +47,9 @@ fn setup(
             y: 0.25,
             z: 0.25,
         }),
-        global_transform: Default::default(),
-        visibility: Default::default(),
-        computed_visibility: Default::default(),
+        ..default()
     });
 
-    // cube
-    // commands.spawn(PbrBundle {
-    //     mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-    //     material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-    //     transform: Transform::from_xyz(0.0, 0.5, 0.0),
-    //     ..default()
-    // });
     // light
     commands.spawn(PointLightBundle {
         point_light: PointLight {
@@ -55,4 +65,37 @@ fn setup(
         transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
+}
+
+fn spawn_player(mut commands: Commands,
+                mut meshes: ResMut<Assets<Mesh>>,
+                mut materials: ResMut<Assets<StandardMaterial>>,) {
+    commands
+        .spawn(InputManagerBundle::<Action> {
+            // Stores "which actions are currently pressed"
+            action_state: ActionState::default(),
+            // Describes how to convert from player inputs into those actions
+            input_map: InputMap::new([(KeyCode::Space, Action::Jump)]),
+        })
+        .insert(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Capsule {
+                radius: 0.1,
+                depth: 0.4,
+                ..default()
+            })),
+            material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+            transform: Transform::from_xyz(0.0, 0.3, 0.5),
+            ..default()
+        })
+        .insert(Player);
+}
+
+// Query for the `ActionState` component in your game logic systems!
+fn jump(mut query: Query<(&ActionState<Action>, &mut Transform), With<Player>>) {
+    let (action_state, mut transform) = query.single_mut();
+    // Each action has a button-like state of its own that you can check
+    if action_state.just_pressed(Action::Jump) {
+        println!("I'm jumping!");
+        transform.translation.y += 0.3;
+    }
 }
