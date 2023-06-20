c6c499762b77bb3b970b92abf0be4e1940974808
ByteBlender <byteblender21@gmail.com>
2023-06-20 23:20:23 +0200

    Fix button text

--- src/ui/menu.rs
+++ src/ui/menu.rs
@@ -57,7 +57,7 @@ const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
 const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
 const PRESSED_BUTTON: Color = Color::rgb(0.35, 0.75, 0.35);
 
-fn render_game_menu(mut commands: Commands, current_state: Res<GameMenu>) {
+fn render_game_menu(mut commands: Commands, asset_server: Res<AssetServer>, current_state: Res<GameMenu>) {
     println!("Activate menu");
     commands
         .spawn(NodeBundle {
@@ -87,6 +87,7 @@ fn render_game_menu(mut commands: Commands, current_state: Res<GameMenu>) {
                     parent.spawn(TextBundle::from_section(
                         "Button",
                         TextStyle {
+                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
                             font_size: 40.0,
                             color: Color::rgb(0.9, 0.9, 0.9),
                             ..default()
